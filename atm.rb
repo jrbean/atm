8943e194256e09cf88d081703ce74ff568566064
Justin Bean <beanjustinr@gmail.com>
2015-09-29 13:47:13 -0400

    adding menu functionality

--- atm.rb
+++ atm.rb
@@ -10,7 +10,7 @@ class ATM
   end
 
   def users
-    CSV.foreach("bank_users.csv", headers: true, header_converters: :symbol, converters: :all) do |row|
+    CSV.foreach("bank_users.csv", headers: true, header_converters: :symbol) do |row|
       @user_data.push row.to_hash
       end
   end
@@ -26,7 +26,6 @@ class ATM
     @user_data.each do |hash|
       if hash[:name] == user_name && hash[:pin].to_s == user_pin
         @user_hash = hash
-        puts @user_hash
       end
     end
   end
@@ -34,17 +33,33 @@ class ATM
   def logged_in?
     @user_hash != nil
   end
-end
 
-a = ATM.new
-
-  a.access_atm
-
-  if a.logged_in?
+  def atm_menu
     puts "Choose an option."
     puts "1 - Check Balance"
     puts "2 - Withdrawal"
     puts "3 - Exit"
+
+    selection = gets.chomp.to_i
+    if selection == 1
+      puts "#{@user_data[:balance].to_i}"
+    elsif selection == 2
+    elsif selection == 3
+      puts "Please come again."
+    else
+      puts "#{selection} is not an available option. Please select from the options available."
+    end
+  end
+end
+
+a = ATM.new
+
+  # loop
+
+  a.access_atm
+
+  if a.logged_in?
+    a.atm_menu
   else
     puts "Invalid name or PIN"
   end
