1415008df949f938dda9b43c8b3f170f585defa0
Justin Bean <beanjustinr@gmail.com>
2015-09-29 09:53:28 -0400

    Fixed log in check

--- atm.rb
+++ atm.rb
@@ -2,7 +2,7 @@ require "csv"
 require "pry"
 
 class ATM
-  attr_accessor :user_data
+  # attr_accessor :user_data
 
   def initialize
     @user_data = []
@@ -11,7 +11,7 @@ class ATM
 
   def users
     CSV.foreach("bank_users.csv", headers: true, header_converters: :symbol, converters: :all) do |row|
-      user_data.push row.to_hash
+      @user_data.push row.to_hash
       end
   end
 
@@ -22,26 +22,29 @@ class ATM
     user_name = gets.chomp
     puts "Please enter your PIN."
     user_pin = gets.chomp
-    user_hash = nil
+    @user_hash = nil
+    @user_data.each do |hash|
+      if hash[:name] == user_name && hash[:pin].to_s == user_pin
+        @user_hash = hash
+        puts @user_hash
+      end
+    end
+  end
+
+  def logged_in?
+    @user_hash != nil
   end
 end
 
 a = ATM.new
 
   a.access_atm
-binding.pry
-  a.user_data.each do |hash|
-    if hash[:name] == user_name && hash[:pin] == user_pin
-      #user_hash = hash
-      puts user_hash
-    end
-  end
 
-  unless user_hash = hash
-    a.access_atm
+  if a.logged_in?
+    puts "Choose an option."
+    puts "1 - Check Balance"
+    puts "2 - Withdrawal"
+    puts "3 - Exit"
+  else
+    puts "Invalid name or PIN"
   end
-
-  puts "Choose an option."
-  puts "1 - Check Balance"
-  puts "2 - Withdrawal"
-  puts "3 - Exit"
